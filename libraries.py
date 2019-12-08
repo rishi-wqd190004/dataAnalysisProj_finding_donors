c24526dedd21cd6d1cfb2c4bd8e58e9be8b05a91
RAJIB KANTI PAL <48209323+rajucsecuet@users.noreply.github.com>
2019-12-09 01:41:32 +0800

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -1,1 +1,35 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+from time import time
+from IPython.display import display
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
+# Load data
+data = pd.read_csv("census.csv")
+
+# Split the data into features and target label
+income_raw = data['income']
+features_raw = data.drop('income', axis = 1)
+
+# Log-transform the skewed features
+skewed = ['capital-gain', 'capital-loss']
+features_log_transformed = pd.DataFrame(data = features_raw)
+features_log_transformed[skewed] = features_raw[skewed].apply(lambda x: np.log(x + 1))
+
+# Normalizing Numerical Features
+scaler = MinMaxScaler() # default=(0, 1)
+numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+features_log_minmax_transform = pd.DataFrame(data = features_log_transformed)
+features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
+
+# Preprocessing Categorial Features
+features_final = pd.get_dummies(features_log_minmax_transform)
+income = income_raw.map({'<=50K':0,'>50K':1})
+
+# Split the 'features' and 'income' data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features_final,income,test_size = 0.2,random_state = 0)
+# Show the results of the split
+print("Training set has {} samples.".format(X_train.shape[0]))
+print("Testing set has {} samples.".format(X_test.shape[0]))
